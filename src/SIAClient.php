96abfcea6136f86379b91ff7ba6f9b1e392b0e04
nikos vavoulas <nikos.va@gmail.com>
2016-12-09 20:47:34 +0200

    Added SIAClient class placeholder

---
+++ src/SIAClient.php
@@ -0,0 +1,19 @@
+<?php
+namespace nikosv\SIA;
+/**
+ * Description of SIAClient
+ *
+ * @author nikos
+ */
+class SIAClient {
+    public static function test()
+    {
+        return 'Everything in the right place!';
+    }
+    public function get(){
+        
+    }
+    public function store(){
+        
+    }
+}
